77c4f8da6b145f59a17a100744a011bc40d59dc4
rjrjr <rjrjr@cox.net>
2010-05-22 21:05:48 +0000

    change user to application_user; changed user_preference to application_user_preference; changed character to comic_character; changed comic_character to comic_character_appearance - rjrjr

--- comic-repository/src/test/java/org/lazydog/comic/internal/repository/ComicRepositoryTest.java
+++ comic-repository/src/test/java/org/lazydog/comic/internal/repository/ComicRepositoryTest.java
@@ -4,13 +4,13 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
 import javax.ejb.embeddable.EJBContainer;
-import javax.naming.Context;
-import javax.naming.InitialContext;
 import org.lazydog.comic.criteria.Criteria;
 import org.lazydog.comic.criteria.CriteriaFactory;
+import org.lazydog.comic.model.ApplicationUser;
+import org.lazydog.comic.model.ApplicationUserPreference;
 import org.lazydog.comic.model.Category;
-import org.lazydog.comic.model.Character;
 import org.lazydog.comic.model.Comic;
+import org.lazydog.comic.model.ComicCharacter;
 import org.lazydog.comic.model.ComicGrade;
 import org.lazydog.comic.model.ComicType;
 import org.lazydog.comic.model.Creator;
@@ -26,8 +26,6 @@ import org.lazydog.comic.model.Publisher;
 import org.lazydog.comic.model.Title;
 import org.lazydog.comic.model.TitleType;
 import org.lazydog.comic.model.Trait;
-import org.lazydog.comic.model.User;
-import org.lazydog.comic.model.UserPreference;
 import org.lazydog.comic.model.Want;
 import org.lazydog.comic.spi.repository.ComicRepository;
 import org.junit.AfterClass;
@@ -43,9 +41,11 @@ import org.junit.Test;
  */
 public class ComicRepositoryTest {
 
+    private static Criteria<ApplicationUser> applicationUserCriteria;
+    private static Criteria<ApplicationUserPreference> applicationUserPreferenceCriteria;
     private static Criteria<Category> categoryCriteria;
-    private static Criteria<Character> characterCriteria;
     private static Criteria<Comic> comicCriteria;
+    private static Criteria<ComicCharacter> comicCharacterCriteria;
     private static Criteria<ComicGrade> comicGradeCriteria;
     private static Criteria<ComicType> comicTypeCriteria;
     private static Criteria<Creator> creatorCriteria;
@@ -61,9 +61,7 @@ public class ComicRepositoryTest {
     private static Criteria<Title> titleCriteria;
     private static Criteria<TitleType> titleTypeCriteria;
     private static Criteria<Trait> traitCriteria;
-    private static Criteria<User> userCriteria;
     private static Criteria<Want> wantCriteria;
-    private static Criteria<UserPreference> userPreferenceCriteria;
     private static ComicRepository comicRepository;
     private static EJBContainer ejbContainer;
     private static SimpleDateFormat sdf;
@@ -72,11 +70,7 @@ public class ComicRepositoryTest {
     public static void initialize() throws Exception {
 
         // Declare.
-        //Context context;
         CriteriaFactory criteriaFactory;
-
-        // Initialize the context.
-        //context = new InitialContext();
 
         // Create the EJB container.
         ejbContainer = EJBContainer.createEJBContainer();
@@ -84,16 +78,15 @@ public class ComicRepositoryTest {
         // Get the comic repository.
         comicRepository = (ComicRepository)ejbContainer.getContext().lookup("ejb/ComicRepository");
 
-        
-        //comicRepository = (ComicRepository)context.lookup("ejb/ComicRepository");
-
         // Get the criteria factory.
         criteriaFactory = CriteriaFactory.instance();
 
         // Initialize the criteria.
+        applicationUserCriteria = criteriaFactory.createCriteria(ApplicationUser.class);
+        applicationUserPreferenceCriteria = criteriaFactory.createCriteria(ApplicationUserPreference.class);
         categoryCriteria = criteriaFactory.createCriteria(Category.class);
-        characterCriteria = criteriaFactory.createCriteria(Character.class);
         comicCriteria = criteriaFactory.createCriteria(Comic.class);
+        comicCharacterCriteria = criteriaFactory.createCriteria(ComicCharacter.class);
         comicGradeCriteria = criteriaFactory.createCriteria(ComicGrade.class);
         comicTypeCriteria = criteriaFactory.createCriteria(ComicType.class);
         creatorCriteria = criteriaFactory.createCriteria(Creator.class);
@@ -109,8 +102,6 @@ public class ComicRepositoryTest {
         titleCriteria = criteriaFactory.createCriteria(Title.class);
         titleTypeCriteria = criteriaFactory.createCriteria(TitleType.class);
         traitCriteria = criteriaFactory.createCriteria(Trait.class);
-        userCriteria = criteriaFactory.createCriteria(User.class);
-        userPreferenceCriteria = criteriaFactory.createCriteria(UserPreference.class);
         wantCriteria = criteriaFactory.createCriteria(Want.class);
 
         // Initialize the data format.
@@ -128,9 +119,11 @@ public class ComicRepositoryTest {
 
         System.out.println(sdf.format(new Date()) + " Fetching all objects ...");
 
+        List<ApplicationUser> applicationUsers = comicRepository.findList(ApplicationUser.class);
+        List<ApplicationUserPreference> applicationUserPreferences = comicRepository.findList(ApplicationUserPreference.class);
         List<Category> categories = comicRepository.findList(Category.class);
-        List<Character> characters = comicRepository.findList(Character.class);
         List<Comic> comics = comicRepository.findList(Comic.class);
+        List<ComicCharacter> comicCharacters = comicRepository.findList(ComicCharacter.class);
         List<ComicGrade> comicGrades = comicRepository.findList(ComicGrade.class);
         List<ComicType> comicTypes = comicRepository.findList(ComicType.class);
         List<Creator> creators = comicRepository.findList(Creator.class);
@@ -146,13 +139,13 @@ public class ComicRepositoryTest {
         List<Title> titles = comicRepository.findList(Title.class);
         List<TitleType> titleTypes = comicRepository.findList(TitleType.class);
         List<Trait> traits = comicRepository.findList(Trait.class);
-        List<User> users = comicRepository.findList(User.class);
-        List<UserPreference> userPreferences = comicRepository.findList(UserPreference.class);
         List<Want> wants = comicRepository.findList(Want.class);
 
+        System.out.println("number of applicationUsers = " + applicationUsers.size());
+        System.out.println("number of applicationUserPreferences = " + applicationUserPreferences.size());
         System.out.println("number of categories = " + categories.size());
-        System.out.println("number of characters = " + characters.size());
         System.out.println("number of comics = " + comics.size());
+        System.out.println("number of comicCharacters = " + comicCharacters.size());
         System.out.println("number of comicGrades = " + comicGrades.size());
         System.out.println("number of comicTypes = " + comicTypes.size());
         System.out.println("number of creators = " + creators.size());
@@ -168,8 +161,6 @@ public class ComicRepositoryTest {
         System.out.println("number of titles = " + titles.size());
         System.out.println("number of titleTypes = " + titleTypes.size());
         System.out.println("number of traits = " + traits.size());
-        System.out.println("number of users = " + users.size());
-        System.out.println("number of userPreferences = " + userPreferences.size());
         System.out.println("number of wants = " + wants.size());
 
         System.out.println(sdf.format(new Date()) + " Finished.");
@@ -180,9 +171,11 @@ public class ComicRepositoryTest {
 
         System.out.println(sdf.format(new Date()) + " Fetching all objects ...");
 
+        List<ApplicationUser> applicationUsers = comicRepository.findList(applicationUserCriteria);
+        List<ApplicationUserPreference> applicationUserPreferences = comicRepository.findList(applicationUserPreferenceCriteria);
         List<Category> categories = comicRepository.findList(categoryCriteria);
-        List<Character> characters = comicRepository.findList(characterCriteria);
         List<Comic> comics = comicRepository.findList(comicCriteria);
+        List<ComicCharacter> comicCharacters = comicRepository.findList(comicCharacterCriteria);
         List<ComicGrade> comicGrades = comicRepository.findList(comicGradeCriteria);
         List<ComicType> comicTypes = comicRepository.findList(comicTypeCriteria);
         List<Creator> creators = comicRepository.findList(creatorCriteria);
@@ -198,13 +191,13 @@ public class ComicRepositoryTest {
         List<Title> titles = comicRepository.findList(titleCriteria);
         List<TitleType> titleTypes = comicRepository.findList(titleTypeCriteria);
         List<Trait> traits = comicRepository.findList(traitCriteria);
-        List<User> users = comicRepository.findList(userCriteria);
-        List<UserPreference> userPreferences = comicRepository.findList(userPreferenceCriteria);
         List<Want> wants = comicRepository.findList(wantCriteria);
 
+        System.out.println("number of applicationUsers = " + applicationUsers.size());
+        System.out.println("number of applicationUserPreferences = " + applicationUserPreferences.size());
         System.out.println("number of categories = " + categories.size());
-        System.out.println("number of characters = " + characters.size());
         System.out.println("number of comics = " + comics.size());
+        System.out.println("number of comicCharacters = " + comicCharacters.size());
         System.out.println("number of comicGrades = " + comicGrades.size());
         System.out.println("number of comicTypes = " + comicTypes.size());
         System.out.println("number of creators = " + creators.size());
@@ -220,8 +213,6 @@ public class ComicRepositoryTest {
         System.out.println("number of titles = " + titles.size());
         System.out.println("number of titleTypes = " + titleTypes.size());
         System.out.println("number of traits = " + traits.size());
-        System.out.println("number of users = " + users.size());
-        System.out.println("number of userPreferences = " + userPreferences.size());
         System.out.println("number of wants = " + wants.size());
 
         System.out.println(sdf.format(new Date()) + " Finished.");
