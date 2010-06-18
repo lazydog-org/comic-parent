6f27f96ecad297a6d6face272fc23453e663cbff
rjrjr <rjrjr@cox.net>
2010-06-18 22:30:07 +0000

    modifications to unit tests - rjrjr

--- comic-repository/src/test/java/org/lazydog/comic/internal/repository/ComicRepositoryTest.java
+++ comic-repository/src/test/java/org/lazydog/comic/internal/repository/ComicRepositoryTest.java
@@ -1,6 +1,7 @@
 package org.lazydog.comic.internal.repository;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import javax.naming.Context;
@@ -8,6 +9,8 @@ import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.lazydog.comic.criteria.Criteria;
 import org.lazydog.comic.criteria.CriteriaFactory;
+import org.lazydog.comic.criteria.criterion.ComparisonOperation;
+import org.lazydog.comic.criteria.criterion.Order;
 import org.lazydog.comic.model.Category;
 import org.lazydog.comic.model.Character;
 import org.lazydog.comic.model.Comic;
@@ -31,6 +34,7 @@ import org.lazydog.comic.model.UserPreference;
 import org.lazydog.comic.model.Want;
 import org.lazydog.comic.spi.repository.ComicRepository;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -113,9 +117,188 @@ public class ComicRepositoryTest {
         wantCriteria = criteriaFactory.createCriteria(Want.class);
 
         // Initialize the data format.
-        sdf = new SimpleDateFormat("MM/DD/yyyy HH:mm:ss");
+        sdf = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
     }
 
+    @Ignore
+    @Test
+    public void findListComic() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Comic> comics;
+            Date endTime;
+            Date startTime;
+
+            for (int x = 0; x < 10; x++) {
+
+                startTime = new Date();
+                System.out.print("Fetching all comics ... ");
+
+                comics = comicRepository.findList(Comic.class);
+
+                endTime = new Date();
+                System.out.println(comics.size() + " retrieved in "
+                        + ((endTime.getTime() - startTime.getTime()) / 1000f)
+                        + " seconds.");
+            }
+        }
+    }
+
+    @Ignore
+    @Test
+    public void findListComicByCriteria() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Comic> comics;
+            Date endTime;
+            Date startTime;
+
+            for (int x = 0; x < 10; x++) {
+                
+                startTime = new Date();
+                System.out.print("Fetching all comics by criteria ... ");
+
+                comics = comicRepository.findList(comicCriteria);
+
+                endTime = new Date();
+                System.out.println(comics.size() + " retrieved in "
+                        + ((endTime.getTime() - startTime.getTime()) / 1000f)
+                        + " seconds.");
+            }
+        }
+    }
+
+    @Ignore
+    @Test
+    public void findListHave() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Have> haves;
+            Date endTime;
+            Date startTime;
+
+            startTime = new Date();
+            System.out.print("Fetching all haves ... ");
+
+            haves = comicRepository.findList(Have.class);
+
+            endTime = new Date();
+            System.out.println(haves.size() + " retrieved in "
+                    + ((endTime.getTime() - startTime.getTime()) / 1000)
+                    + " seconds.");
+        }
+    }
+
+    @Ignore
+    @Test
+    public void findListHaveByCriteria() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Have> haves;
+            Date endTime;
+            Date startTime;
+
+            startTime = new Date();
+            System.out.print("Fetching all haves by criteria ... ");
+
+            haves = comicRepository.findList(haveCriteria);
+
+            endTime = new Date();
+            System.out.println(haves.size() + " retrieved in "
+                    + ((endTime.getTime() - startTime.getTime()) / 1000)
+                    + " seconds.");
+        }
+    }
+
+    @Test
+    public void findListHaveBySpecificCriteria() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Have> haves;
+            User user;
+
+            userCriteria.add(ComparisonOperation.eq("name", "rjrjr"));
+            user = comicRepository.find(userCriteria);
+
+            haveCriteria.add(ComparisonOperation.eq("createUser", user));
+            haveCriteria.addOrder(Order.asc("comic.title.name"));
+
+            haves = comicRepository.findList(haveCriteria);
+
+            if (haves != null) {
+
+                List<Title> titles;
+
+                titles = new ArrayList<Title>();
+
+                for (Have have : haves) {
+
+                    if (!titles.contains(have.getComic().getTitle())) {
+
+                        titles.add(have.getComic().getTitle());
+                    }
+                }
+            }
+        }
+    }
+    
+    @Ignore
+    @Test
+    public void findListTitle() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Title> titles;
+            Date endTime;
+            Date startTime;
+
+            startTime = new Date();
+            System.out.print("Fetching all titles ... ");
+
+            titles = comicRepository.findList(Title.class);
+
+            endTime = new Date();
+            System.out.println(titles.size() + " retrieved in "
+                    + ((endTime.getTime() - startTime.getTime()) / 1000)
+                    + " seconds.");
+        }
+    }
+
+    @Ignore
+    @Test
+    public void findListTitleByCriteria() throws Exception {
+
+        // Check if the comic repository is accessible.
+        if (comicRepository != null) {
+
+            List<Title> titles;
+            Date endTime;
+            Date startTime;
+
+            startTime = new Date();
+            System.out.print("Fetching all titles by criteria ... ");
+
+            titles = comicRepository.findList(titleCriteria);
+
+            endTime = new Date();
+            System.out.println(titles.size() + " retrieved in "
+                    + ((endTime.getTime() - startTime.getTime()) / 1000)
+                    + " seconds.");
+        }
+    }
+
+    @Ignore
     @Test
     public void findList() throws Exception {
 
@@ -126,12 +309,10 @@ public class ComicRepositoryTest {
 
             List<Category> categories = comicRepository.findList(Category.class);
             List<Character> characters = comicRepository.findList(Character.class);
-            List<Comic> comics = comicRepository.findList(Comic.class);
             List<ComicGrade> comicGrades = comicRepository.findList(ComicGrade.class);
             List<ComicType> comicTypes = comicRepository.findList(ComicType.class);
             List<Creator> creators = comicRepository.findList(Creator.class);
             List<Distribution> distributions = comicRepository.findList(Distribution.class);
-            List<Have> haves = comicRepository.findList(Have.class);
             List<Image> images = comicRepository.findList(Image.class);
             List<ImageType> imageTypes = comicRepository.findList(ImageType.class);
             List<Imprint> imprints = comicRepository.findList(Imprint.class);
@@ -139,7 +320,6 @@ public class ComicRepositoryTest {
             List<Person> persons = comicRepository.findList(Person.class);
             List<Profession> professions = comicRepository.findList(Profession.class);
             List<Publisher> publishers = comicRepository.findList(Publisher.class);
-            List<Title> titles = comicRepository.findList(Title.class);
             List<TitleType> titleTypes = comicRepository.findList(TitleType.class);
             List<Trait> traits = comicRepository.findList(Trait.class);
             List<User> users = comicRepository.findList(User.class);
@@ -148,12 +328,10 @@ public class ComicRepositoryTest {
 
             System.out.println("number of categories = " + categories.size());
             System.out.println("number of characters = " + characters.size());
-            System.out.println("number of comics = " + comics.size());
             System.out.println("number of comicGrades = " + comicGrades.size());
             System.out.println("number of comicTypes = " + comicTypes.size());
             System.out.println("number of creators = " + creators.size());
             System.out.println("number of distributions = " + distributions.size());
-            System.out.println("number of haves = " + haves.size());
             System.out.println("number of images = " + images.size());
             System.out.println("number of imageTypes = " + imageTypes.size());
             System.out.println("number of imprint = " + imprints.size());
@@ -161,7 +339,6 @@ public class ComicRepositoryTest {
             System.out.println("number of persons = " + persons.size());
             System.out.println("number of professions = " + professions.size());
             System.out.println("number of publishers = " + publishers.size());
-            System.out.println("number of titles = " + titles.size());
             System.out.println("number of titleTypes = " + titleTypes.size());
             System.out.println("number of traits = " + traits.size());
             System.out.println("number of users = " + users.size());
@@ -172,6 +349,7 @@ public class ComicRepositoryTest {
         }
     }
 
+    @Ignore
     @Test
     public void findListByCriteria() throws Exception {
 
@@ -182,12 +360,10 @@ public class ComicRepositoryTest {
 
             List<Category> categories = comicRepository.findList(categoryCriteria);
             List<Character> characters = comicRepository.findList(characterCriteria);
-            List<Comic> comics = comicRepository.findList(comicCriteria);
             List<ComicGrade> comicGrades = comicRepository.findList(comicGradeCriteria);
             List<ComicType> comicTypes = comicRepository.findList(comicTypeCriteria);
             List<Creator> creators = comicRepository.findList(creatorCriteria);
             List<Distribution> distributions = comicRepository.findList(distributionCriteria);
-            List<Have> haves = comicRepository.findList(haveCriteria);
             List<Image> images = comicRepository.findList(imageCriteria);
             List<ImageType> imageTypes = comicRepository.findList(imageTypeCriteria);
             List<Imprint> imprints = comicRepository.findList(imprintCriteria);
@@ -195,7 +371,6 @@ public class ComicRepositoryTest {
             List<Person> persons = comicRepository.findList(personCriteria);
             List<Profession> professions = comicRepository.findList(professionCriteria);
             List<Publisher> publishers = comicRepository.findList(publisherCriteria);
-            List<Title> titles = comicRepository.findList(titleCriteria);
             List<TitleType> titleTypes = comicRepository.findList(titleTypeCriteria);
             List<Trait> traits = comicRepository.findList(traitCriteria);
             List<User> users = comicRepository.findList(userCriteria);
@@ -204,12 +379,10 @@ public class ComicRepositoryTest {
 
             System.out.println("number of categories = " + categories.size());
             System.out.println("number of characters = " + characters.size());
-            System.out.println("number of comics = " + comics.size());
             System.out.println("number of comicGrades = " + comicGrades.size());
             System.out.println("number of comicTypes = " + comicTypes.size());
             System.out.println("number of creators = " + creators.size());
             System.out.println("number of distributions = " + distributions.size());
-            System.out.println("number of haves = " + haves.size());
             System.out.println("number of images = " + images.size());
             System.out.println("number of imageTypes = " + imageTypes.size());
             System.out.println("number of imprints = " + imprints.size());
@@ -217,7 +390,6 @@ public class ComicRepositoryTest {
             System.out.println("number of persons = " + persons.size());
             System.out.println("number of professions = " + professions.size());
             System.out.println("number of publishers = " + publishers.size());
-            System.out.println("number of titles = " + titles.size());
             System.out.println("number of titleTypes = " + titleTypes.size());
             System.out.println("number of traits = " + traits.size());
             System.out.println("number of users = " + users.size());
