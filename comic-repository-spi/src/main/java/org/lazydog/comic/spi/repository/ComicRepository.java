09f14dee6f3446a8f2b91f669d7e219cb2478640
rjrjr <rjrjr@cox.net>
2010-08-20 15:33:35 +0000

    modifications to use Data Access API - rjrjr

--- comic-repository-spi/src/main/java/org/lazydog/comic/spi/repository/ComicRepository.java
+++ comic-repository-spi/src/main/java/org/lazydog/comic/spi/repository/ComicRepository.java
@@ -1,8 +1,6 @@
 package org.lazydog.comic.spi.repository;
 
-import java.util.List;
-import org.lazydog.comic.model.Entity;
-import org.lazydog.comic.criteria.Criteria;
+import org.lazydog.data.access.DataAccessObject;
 
 
 /**
@@ -10,21 +8,5 @@ import org.lazydog.comic.criteria.Criteria;
  *
  * @author  Ron Rickard
  */
-public interface ComicRepository {
-
-    public <T extends Entity<T>> T find(Class<T> entityClass, Integer id);
-
-    public <T extends Entity<T>> T find(Criteria<T> criteria);
-
-    public <T extends Entity<T>> List<T> findList(Class<T> entityClass);
-    
-    public <T extends Entity<T>> List<T> findList(Criteria<T> criteria);
-
-    public <T extends Entity<T>> T persist(T entity);
-    
-    public <T extends Entity<T>> List<T> persistList(List<T> entities);
-
-    public <T extends Entity<T>> void remove(T entity);
-    
-    public <T extends Entity<T>> void remove(Class<T> entityClass, Integer id);
+public interface ComicRepository extends DataAccessObject {
 }
