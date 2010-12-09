3817a18c697bf6af257b98a9c12a64d3354b2b9e
rjrjr <rjrjr@cox.net>
2010-12-09 22:08:46 +0000

    start work on month year picker - rjrjr

---
+++ test-web/src/main/java/org/lazydog/comic/bean/TestBean.java
@@ -0,0 +1,21 @@
+package org.lazydog.comic.bean;
+
+import java.util.Date;
+
+
+/**
+ *
+ * @author R4R
+ */
+public class TestBean {
+
+    private Date monthYear;
+
+    public Date getMonthYear() {
+        return this.monthYear;
+    }
+    
+    public void setMonthYear(Date monthYear) {
+        this.monthYear = monthYear;
+    }
+}
