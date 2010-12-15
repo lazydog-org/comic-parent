61f17a45d3881a4e76d5599a7509dcad95e72ea8
rjrjr <rjrjr@cox.net>
2010-12-15 07:17:59 +0000

    more work on month/year picker - rjrjr

--- test-web/src/main/java/org/lazydog/comic/bean/TestBean.java
+++ test-web/src/main/java/org/lazydog/comic/bean/TestBean.java
@@ -9,13 +9,31 @@ import java.util.Date;
  */
 public class TestBean {
 
+    private Date endMonthYear;
     private Date monthYear;
+    private Date startMonthYear;
+
+    public Date getEndMonthYear() {
+        return this.endMonthYear;
+    }
 
     public Date getMonthYear() {
         return this.monthYear;
     }
-    
+
+    public Date getStartMonthYear() {
+        return this.startMonthYear;
+    }
+
+    public void setEndMonthYear(Date endMonthYear) {
+        this.endMonthYear = endMonthYear;
+    }
+
     public void setMonthYear(Date monthYear) {
         this.monthYear = monthYear;
+    }
+
+    public void setStartMonthYear(Date startMonthYear) {
+        this.startMonthYear = startMonthYear;
     }
 }
